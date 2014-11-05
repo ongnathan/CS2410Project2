83638d6f38a34f45e47799ace4985ad065f09405
Nathan O <nro5@pitt.edu>
2014-11-05 18:55:27 -0500

    Added a processor object.  Each L1 cache should create one of these.

---
+++ src/processor/Processor.java
@@ -0,0 +1,58 @@
+package processor;
+
+/**
+ * 
+ * @author Nathan Ong
+ *
+ */
+public class Processor
+{
+	private static int processorNumberCounter = 0;
+	public final int processorNumber;
+	
+	private int cycleNumber;
+	private boolean isWriteCommand;
+	private long address;
+	
+	public Processor()
+	{
+		this.processorNumber = processorNumberCounter;
+		this.cycleNumber = -1;
+		this.isWriteCommand = false;
+		this.address = -1L;
+		processorNumberCounter++;
+	}
+	
+	/**
+	 * 
+	 * @param instruction
+	 * @return Returns whether or not the instruction could be parsed.  Returns false if the instruction is not meant for this processor.
+	 */
+	public boolean parseInstruction(String instruction)
+	{
+		String[] tokens = instruction.trim().split("\t");
+		if(Integer.parseInt(tokens[1]) == this.processorNumber)
+		{
+			return false;
+		}
+		this.cycleNumber = Integer.parseInt(tokens[0]);
+		this.isWriteCommand = Integer.parseInt(tokens[2]) == 1;
+		this.address = Long.parseLong(tokens[3], 16);
+		return true;
+	}
+	
+	public int getInstructionCycleNumber()
+	{
+		return this.cycleNumber;
+	}
+	
+	public boolean isInstructionWriteCommand()
+	{
+		return this.isWriteCommand;
+	}
+	
+	public long getInstructionAddress()
+	{
+		return this.address;
+	}
+}//end class
